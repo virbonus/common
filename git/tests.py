92b90cf45763be3e39b1909fd5788339adf613b2
Ihor Hryhola <grygola@gmail.com>
2019-11-27 17:21:59 +0200

    Adapted for local usage

--- git/tests.py
+++ git/tests.py
@@ -1,7 +1,8 @@
 import unittest
 import random
 
-from base_types_exceptions_loops.homework import (
+#from base_types_exceptions_loops.homework import (
+from homework import (
     is_two_object_has_same_value,
     is_two_objects_has_same_type,
     is_two_objects_is_the_same_objects,
